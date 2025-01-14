e155494d688ea594e9cfdc964f845234e1de6850
Alexander-lll <alexanderdrivas@icloud.com>
2025-01-13 20:52:16 -0800

    Update TypingAnimation.tsx

--- src/components/TypingAnimation.tsx
+++ src/components/TypingAnimation.tsx
@@ -4,7 +4,7 @@ import React from "react";
 import { TypeAnimation } from "react-type-animation";
 
 const TypingAnimation = () => {
-    return <TypeAnimation sequence={["Machine Learning Engineer 💻", 1000, "HuggingFace Fellow 🤗", 1000, "Open Source Contributor 📖", 1000]} wrapper="span" speed={50} className="font-bold lg:text-3xl text-blue-500" repeat={Infinity} />;
+    return <TypeAnimation sequence={["Machine Learning Engineer 💻", 1000, "Founder 👨🏽‍🎨", 1000, "Open Source Contributor 📖", 1000]} wrapper="span" speed={50} className="font-bold lg:text-3xl text-blue-500" repeat={Infinity} />;
 };
 
 export default TypingAnimation;
